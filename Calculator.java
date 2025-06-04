b3e1740ddd254344e05e160318f55055579b755a
Yasine Mouadab <yassinemouaddab@MacBookPro.lan>
2025-06-03 21:42:53 -0400

    new java class

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,12 @@
 public class Calculator {
     public static void main(String args[]){
 
-        System.out.print("Hello World");
+        System.out.print("the result are: ");
+        int result = add(2,5);
+        System.out.print(result);
+    }
 
+    public static int add(int a, int b){
+        return a+b;
     }
 }
