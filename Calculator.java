96a086d62701618c6ac8a3f249151e058c55c824
yassinemouaddab <m.yassine.bc@gmail.com>
2025-06-03 21:48:27 -0400

    new java class

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,13 @@
+import java.util.*;
 public class Calculator {
     public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
         System.out.print("the result are: ");
-        int result = add(2,5);
+        int result = add(a,b);
         System.out.print(result);
     }
 
